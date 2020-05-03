6996fda1bc970d1fc04d55fbfaf9913fc87f82b5
Airels <yohan13240@hotmail.fr>
2020-05-03 20:40:41 +0200

    Modified main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,32 +6,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        Scanner reader = new Scanner(System.in);
-
-        System.out.println("Result to find ?");
-        int result = Integer.parseInt(reader.nextLine());
-
-        // To respect TV show: 6 numbers, 9 first numbers, + 10, 25, 50, 75 and 100
-        System.out.println("\nWhat numbers to use to resolve result ? (spaced with space)");
-        String numbersEntered = reader.nextLine();
-        List<Integer> numbers = new ArrayList<>();
-        for (String number : numbersEntered.split(" "))
-            numbers.add(Integer.parseInt(number));
+        int result = 663;
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(50, 25, 3, 9, 4, 1));
 
         List<String> operationsMade = Resolver.naiveResolve(result, numbers);
-
         for (String operationMade : operationsMade) {
             System.out.println(operationMade);
         }
 
-         */
-
-        int result = 663;
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(50, 25, 3, 9, 4, 1));
-        List<String> operationsMade = Resolver.recursiveResolve(result, numbers);
-
-        for (String operationMade : operationsMade) {
+        List<String> operationsMade2 = Resolver.recursiveResolve(result, numbers);
+        for (String operationMade : operationsMade2) {
             System.out.println(operationMade);
         }
     }
