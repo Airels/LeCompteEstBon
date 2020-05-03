320260e15b5a6cbae7fbc41303620c56945b5a50
Airels <yohan13240@hotmail.fr>
2020-05-03 17:03:54 +0200

    Added exception

---
+++ src/main/java/fr/airels/lecompteestbon/exceptions/NonIntegerValueException.java
@@ -0,0 +1,7 @@
+package fr.airels.lecompteestbon.exceptions;
+
+public class NonIntegerValueException extends Exception {
+    public NonIntegerValueException(String errorMessage) {
+        super(errorMessage);
+    }
+}
